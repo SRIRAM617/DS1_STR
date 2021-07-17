e1f2c0c7ab9fea7e11ab93e43501ff72b58013e5
SRIRAM617 <65893800+SRIRAM617@users.noreply.github.com>
2021-07-17 15:32:53 +0530

    Create LengthOfAString.c

---
+++ LengthOfAString.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char name[]="Welcome";
+    int i;
+    for(i=0;name[i]!='\0';i++)
+    {
+        
+    }
+    printf("The length of the string was %d \n",i);
+}
